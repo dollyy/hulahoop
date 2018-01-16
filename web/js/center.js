92d933066ff7d7987f16b232761fa3a16579a29d
yangchen1009 <yangchen1009@thundersoft.com>
2018-01-16 18:45:23 +0800

    add pie

---
+++ web/js/center.js
@@ -0,0 +1,183 @@
+//todo 1.highlight color of province? 2.the gradient color of map
+$(function(){
+    var myChart,data;
+    $(window).resize(function(){
+        myChart.resize();
+    });
+    $("#mapDot").click(function(){
+        $("#strategyMap").css("display","block");
+        $("#strategyPie").css("display","none");
+    });
+    $("#pieDot").click(function(){
+        $("#strategyMap").css("display","none");
+        $("#strategyPie").css("display","block");
+        showPie(data);
+    });
+    
+    $.ajax({
+/*        type:"post",
+        url:"",
+        dataType:"json",*/
+        success:function(){
+/*            var data={"userMap":[[1,7],[2,11],[3,15],[4,12],[5,7],[6,10],[7,2],[8,11],[9,2],[10,1],[11,5],[12,1],[13,3],[14,11],[15,2],[16,1],[17,5],[18,1],[19,2],[20,11],[21,11],[22,4],[23,5],[24,1],[25,2],[26,11],[27,1],[28,1],[29,5],[30,1],[31,2],[32,11],[34,1],[35,9]]};*/
+            var datas={"userMap":[[1,15,'安徽'],[2,2,'澳门'],[3,1,'北京'],[4,2,'重庆'],[5,1,'福建'],[6,1,'吉林'],[8,8,'江西'],[12,2,'黑龙江'],[15,2,'甘肃'],[18,2,'贵州'],[20,1,'南海诸岛'],[23,8,'青海'],[27,4,'上海'],[35,4,'浙江']]};
+            data=datas;
+            showMap(datas);
+        },
+        error:function(){
+            alert("init error");
+        }
+    });
+    
+    function showPie(data){
+        var province=new Array();
+        for(i=0;i<data.userMap.length;i++){
+            province[i]=data.userMap[i][2];
+        }
+        myChart=echarts.init(document.getElementById("strategyPie"));
+        option = {
+            tooltip : {
+                trigger: 'item',
+                formatter: "{b} : {c} ({d}%)"
+            },
+            legend: {
+                orient: 'vertical',
+                bottom: 'bottom',
+                data: province
+            },
+            series : [
+            {
+                name: '',
+                type: 'pie',
+                radius : ['20%','55%'],
+                center: ['50%', '50%'],
+                data:[
+                    {value: 15, name: province[0]},
+                    {value: 2, name: province[1]},
+                    {value: 1, name: province[2]},
+                    {value: 2, name: province[3]},
+                    {value: 1, name: province[4]},
+                    {value: 1, name: province[5]},
+                    {value: 8, name: province[6]},
+                    {value: 2, name: province[7]},
+                    {value: 2, name: province[8]},
+                    {value: 2, name: province[9]},
+                    {value: 1, name: province[10]},
+                    {value: 8, name: province[11]},
+                    {value: 4, name: province[12]},
+                    {value: 4, name: province[13]}
+                ],
+                itemStyle: {
+                    emphasis: {
+                        shadowBlur: 10,
+                        shadowOffsetX: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                }
+            }
+        ]
+        };
+        myChart.setOption(option);
+    }
+    
+    function showMap(datas){
+        var color=new Array();
+        var i;
+        for(i = 0; i < datas.userMap.length; i++){
+            color[datas.userMap[i][0]-1]=datas.userMap[i][1];
+        }
+        for(i=0; i< 35; i++){
+            (typeof(color[i]) == "undefined") ? color[i]=0 : ""
+        }
+        myChart=echarts.init(document.getElementById("strategyMap"));
+        option = {
+            tooltip:{
+                formatter:"{b} : {c}"
+            },
+            visualMap: {
+                min: 0,
+                max: 20,
+                left: 'left',
+                top: 'bottom',
+                //text: ['High','Low'],
+                seriesIndex: [1],   //collect data from series
+                inRange: {
+                    color: ['#f0f0f0', '#0A1451']
+                },
+                calculable : true   //triangle and the limit number
+            },
+            geo: {
+                map: 'china',
+                roam: 'scale',
+                scaleLimit:{
+                    max:1.2,
+                    min:0.8
+                },
+                label:{
+                    emphasis:{
+                        show:false,
+                    }
+                },
+                itemStyle: {
+                    normal:{
+                        borderColor: 'transparent',
+                    },
+                    emphasis:{
+                        shadowBlur: 20,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)',
+                        color:'#0A1451', //highlight color
+                    }
+                }
+            },
+            series : [
+            {
+               type: 'scatter',
+               coordinateSystem: 'geo',
+               symbolSize: 0,
+            },
+            {
+                name: 'color of each province',
+                type: 'map',
+                geoIndex: 0,
+                data:[
+                    {name: '安徽', value: color[0]},
+                    {name: '澳门', value: color[1]},
+                    {name: '北京', value: color[2]},
+                    {name: '重庆', value: color[3]},
+                    {name: '福建', value: color[4]},
+                    {name: '吉林', value: color[5]},
+                    {name: '江苏', value: color[6]},
+                    {name: '江西', value: color[7]},
+                    {name: '海南', value: color[8]},
+                    {name: '河北', value: color[9]},
+                    {name: '河南', value: color[10]},
+                    {name: '黑龙江', value: color[11]},
+                    {name: '湖北', value: color[12]},
+                    {name: '湖南', value: color[13]},
+                    {name: '甘肃', value: color[14]},
+                    {name: '广东', value: color[15]},
+                    {name: '广西', value: color[16]},
+                    {name: '贵州', value: color[17]},
+                    {name: '辽宁', value: color[18]},
+                    {name: '南海诸岛', value: color[19]},
+                    {name: '内蒙古', value: color[20]},
+                    {name: '宁夏', value: color[21]},
+                    {name: '青海', value: color[22]},
+                    {name: '山东', value: color[23]},
+                    {name: '山西', value: color[24]},
+                    {name: '陕西', value: color[25]},
+                    {name: '上海', value: color[26]},
+                    {name: '四川', value: color[27]},
+                    {name: '台湾', value: color[28]},
+                    {name: '天津', value: color[29]},
+                    {name: '西藏', value: color[30]},
+                    {name: '香港', value: color[31]},
+                    {name: '新疆', value: color[32]},
+                    {name: '云南', value: color[33]},
+                    {name: '浙江', value: color[34]}
+                ]
+            }]
+        };
+        myChart.setOption(option);
+    }
+});
---
+++ web/js/comment.js
@@ -0,0 +1,186 @@
+var level;   //level of each comment; 
+$(function(){
+    
+    //list all the comments --start
+    $.ajax({
+/*        type:"post",
+        url:"",
+        dataType:"json",*/
+        success:function(data){
+            //comments' level,responseImg,responseUid,responseName,requestUid,requestName,recontent,date,forNum,againstNum
+            var data={"comments":[
+                [
+                    ["1","1","1","user1","0","0","今天是几号呀","2017-12-11 12:11:11",'1','2'],
+                    ["1.1","2","2","user2","1","user1","15号","2017-12-11 12:11:12",'8','7'],
+                    ["1.1.1","3","3","user3","2","user2","哈哈","2017-12-11 12:11:13",'5','2'],
+                    ["1.1.2","1","1","user1","2","user2","好嘞","2017-12-11 12:11:14",'7','4'],
+                    ["1.2","3","3","user3","1","user1","12.15","2017-12-11 12:11:13",'1','2'],
+                    ["1.3","4","4","user4","1","user1","不造啊","2017-12-11 12:11:16",'4','6'],
+                    ["1.4","5","5","user5","1","user1","hhhh????","2017-12-11 12:11:17",'1','0']
+                ],
+                [
+                    ["2","1","1","user1","0","0","明天是几号啊","2017-12-11 12:11:12",'1','2'],
+                    ["2.1","2","2","user2","1","user1","12.16","2017-12-11 12:11:13",'6','9'],
+                    ["2.1.1","3","3","user3","2","user2","嘿嘿","2017-12-11 12:11:14",'6','2'],
+                    ["2.1.2","1","1","user1","2","user2","造了","2017-12-11 12:11:15",'1','3'],
+                    ["2.2","3","3","user3","3","user1","16号","2017-12-11 12:11:14",'8','2'],
+                    ["2.3","4","4","user4","4","user1","emmmm","2017-12-11 12:11:18",'0','4']
+                ]
+            ]};
+            console.log(JSON.stringify(data));
+            
+            var commentPadding;   //padding-left of each comment
+            for(var i=0;i<data.comments.length;i++){
+                //add comment div,in order to tell the different comment div, I put a variable i after comment
+                $("#comments").append("<div class='comment"+i+"'></div>");
+                for(var j=0;j<data.comments[i].length;j++){
+                    level=data.comments[i][j][0];
+                    commentPadding=(level.indexOf(".") != -1) ? 40*(level.split(".").length-1) : 0; //calculate the padding-left                    
+                    //add each div
+                    $(".comment"+i).append("<div class='each' level='"+level+"'><div><div class='user' style='padding-left: "+commentPadding+"px'><img src='../images/icon"+data.comments[i][j][1]+".jpg'><span class='responseUid' uid='"+data.comments[i][j][2]+"'>"+data.comments[i][j][3]+"</span></div><div class='content'><div><span class='response'>"+(data.comments[i][j][4] == 0 ? "" : "@"+data.comments[i][j][5])+"</span><span>"+data.comments[i][j][6]+"</span></div><div class='operation'><span class='commentTime'>"+data.comments[i][j][7]+"</span><span class='addComment'>回复</span><i class='iconfont icon-zan1'>"+data.comments[i][j][8]+"</i><i class='iconfont icon-zan11'>"+data.comments[i][j][9]+"</i></div></div></div><div class='commentContent' style='padding-left: "+commentPadding+"px'><div><span class='currentUser'>currentuser</span><textarea class='currentComment'></textarea></div><div class='btns'><input type='button' class='btnAdd' value='add'><input type='button' class='btnCancle' value='cancle'></div></div></div>");
+                    
+                    //click 回复 to show the addComment div
+                    $(".addComment").unbind("click").bind("click",showAddComment);
+                    
+                    //click cancle to hide the addComment div
+                    $(".btnCancle").unbind("click").bind("click",hideAddComment);
+                    
+                    //click add to add comment
+                    $(".btnAdd").unbind("click").bind("click",addComment);
+                    
+                    //click against, add against number
+                    $(".icon-zan1").unbind("click").bind("click",operateNumber);
+                    
+                    //click for, add for number
+                    $(".icon-zan11").unbind("click").bind("click",operateNumber);
+                }
+            }
+        },
+        error:function(){
+            //alert("error");
+        }
+    });
+    //list all the comments --end
+    
+});
+
+//show addComment div --start
+function showAddComment(){
+    $(this).parent().parent().parent().next().show();
+}
+//show addComment div --end
+
+//hide addComment div --start
+function hideAddComment(){
+    $(this).parent().parent().hide();
+    $(this).parent().prev().find(".currentComment").val("");    //clean the value of comment textarea
+}
+//hide addComment div --end
+
+//add comment --start
+function addComment(){
+    var that=$(this);
+    var level=$(this).parent().parent().parent().attr("level");
+    var responseUid="6";
+    var responseName="user6";
+    var responseImg="6";
+    var requestUid=$(this).parent().parent().parent().find(".responseUid").attr("uid");
+    var requestName=$(this).parent().parent().parent().find(".responseUid").html();
+    var comment=$(this).parent().prev().find(".currentComment").val();
+    console.log(level+","+responseUid+","+responseName+","+requestUid+","+requestName+","+comment);
+    $.ajax({
+/*            type:"post",
+        url:"",
+        data:{"level":level,"responseUid":responseUid,"comment":comment},
+        dataType:"text",*/
+        success:function(data){
+            var data=12;
+            commentPadding=parseInt(that.parent().parent().css("padding-left")) + 40;  //calculate the padding-left
+            //add comment on the web                
+            that.parent().parent().parent().after("<div class='each' level='"+data+"'><div><div class='user' style='padding-left: "+commentPadding+"px'><img src='../images/icon"+responseImg+".jpg'><span class='responseUid' uid='"+responseUid+"'>"+responseName+"</span></div><div class='content'><div><span class='response'>@"+requestName+"</span><span>"+comment+"</span></div><div class='operation'><span class='commentTime'>"+currentTime()+"</span><span class='addComment'>回复</span><i class='iconfont icon-zan1'>0</i><i class='iconfont icon-zan11'>0</i></div></div></div><div class='commentContent' style='padding-left: "+commentPadding+"px'><div><span class='currentUser'>currentuser</span><textarea class='currentComment'></textarea></div><div class='btns'><input type='button' class='btnAdd' value='add'><input type='button' class='btnCancle' value='cancle'></div></div></div>");
+
+            //clean the comment textarea
+            that.parent().parent().hide();
+            that.parent().prev().find(".currentComment").val("");
+
+            //click 回复 to show the addComment div
+            $(".addComment").unbind("click").bind("click",showAddComment);
+
+            //click cancle to hide the addComment div
+            $(".btnCancle").unbind("click").bind("click",hideAddComment);
+
+            //click add to add comment
+            $(".btnAdd").unbind("click").bind("click",addComment);
+
+            //click against, add against number
+            $(".icon-zan1").unbind("click").bind("click",operateAgainst);
+
+            //click for, add for number
+            $(".icon-zan11").unbind("click").bind("click",operateFor);
+        },
+        error:function(){
+            //alert("error");
+        }
+    });
+}
+//add comment --end
+
+//calculate current time --start
+function currentTime(){
+    var time=new Date();
+    var month=formatNum(time.getMonth()+1);
+    var day=formatNum(time.getDate());
+    var hour=formatNum(time.getHours());
+    var min=formatNum(time.getMinutes());
+    var second=formatNum(time.getSeconds());
+    return time.getFullYear()+"-"+month+"-"+day+" "+hour+":"+min+":"+second;
+}
+//calculate current time --end
+
+//format number --start
+function formatNum(num){
+    if(1 == num.toString().split("").length){
+        return "0"+num;
+    }
+    return num;
+}
+//format number --end
+
+//operate for or against --start
+function operateNumber(){
+    var prevClass=$(this).prev().attr("class");
+    var nextClass=$(this).next().attr("class");
+    if(nextClass && (nextClass.indexOf("colorBlack") != -1)){
+        $(".tips").html("already clicked against").show().fadeOut(2000);
+        return;
+    }else if(prevClass && (prevClass.indexOf("colorBlack") != -1)){
+        $(".tips").html("already clicked for").show().fadeOut(2000);
+        return;
+    }
+    var number=$(this).html();
+    if($(this).attr("class").indexOf("colorBlack") != -1){
+        $(this).removeClass("colorBlack");
+        $(this).html(--number);
+    }else{
+        $(this).addClass("colorBlack");
+        $(this).html(++number);
+    }
+    var numberType="for";
+    if($(this).attr("class").indexOf("icon-zan11") != -1){
+        numberType="against";
+    }
+    level=$(this).parent().parent().parent().parent().attr("level");
+    console.log(level+","+numberType+","+number);
+    $.ajax({
+/*        type:"post",
+        url:"",
+        data:{"level":level,"":number},*/
+        success:function(){
+            
+        },
+        error:function(){
+            //alert("error");
+        }
+    });
+}
+//operate for or against --end
