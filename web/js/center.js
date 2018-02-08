07b628be7cc7a977043148ea1f83e77e71b86ffc
yangchen1009 <yangchen1009@thundersoft.com>
2018-02-08 18:28:45 +0800

    update center.htm

--- web/js/center.js
+++ web/js/center.js
@@ -1,7 +1,14 @@
 //todo 
 //1.highlight color of province?
 //2.the gradient color of map
+//3.the format of phone
+//4.whether city is needed?
 $(function(){
+    
+    //select2
+    $(document).ready(function() {
+        $('.js-example-basic-single').select2();
+    });
     
     $.ajax({
 /*        type:"post",
@@ -9,18 +16,47 @@ $(function(){
         dataType:"json",*/
         success:function(data){
 /*            var data={"userMap":[[1,7],[2,11],[3,15],[4,12],[5,7],[6,10],[7,2],[8,11],[9,2],[10,1],[11,5],[12,1],[13,3],[14,11],[15,2],[16,1],[17,5],[18,1],[19,2],[20,11],[21,11],[22,4],[23,5],[24,1],[25,2],[26,11],[27,1],[28,1],[29,5],[30,1],[31,2],[32,11],[34,1],[35,9]]};*/
-            var datas={"userMap":[[1,15,'安徽'],[2,2,'澳门'],[3,1,'北京'],[4,2,'重庆'],[5,1,'福建'],[6,1,'吉林'],[8,8,'江西'],[12,2,'黑龙江'],[15,2,'甘肃'],[18,2,'贵州'],[20,1,'南海诸岛'],[23,8,'青海'],[27,4,'上海'],[35,4,'浙江']],"stStrategy":[[["1","../images/icons/icon1.jpg","123"],["2","../images/icons/icon2.jpg","123"],["3","../images/icons/icon3.jpg","123"],["4","../images/icons/icon4.jpg","123"],["5","../images/icons/icon5.jpg","123"],["6","../images/icons/icon6.jpg","123"],["7","../images/notfound.jpg","123"],["8","../images/systemerror.jpg","123"]],[["1","../images/icons/icon1.jpg","123"],["2","../images/icons/icon2.jpg","123"],["3","../images/icons/icon3.jpg","123"],["4","../images/icons/icon4.jpg","123"],["5","../images/icons/icon5.jpg","123"],["6","../images/icons/icon6.jpg","123"],["7","../images/notfound.jpg","123"],["8","../images/systemerror.jpg","123"]]]};
+            var datas={"userMap":[[1,15,'安徽'],[2,2,'澳门'],[3,1,'北京'],[4,2,'重庆'],[5,1,'福建'],[6,1,'吉林'],[8,8,'江西'],[12,2,'黑龙江'],[15,2,'甘肃'],[18,2,'贵州'],[20,1,'南海诸岛'],[23,8,'青海'],[27,4,'上海'],[35,4,'浙江']],"stStrategy":[
+                [
+                    ["1","../images/icons/icon1.jpg","1"],["2","../images/icons/icon2.jpg","2"],["3","../images/icons/icon3.jpg","3"],["4","../images/icons/icon4.jpg","4"]
+                ],[
+                    ["1","../images/icons/icon1.jpg","1"],["2","../images/icons/icon2.jpg","2"]
+                ],[
+                    ["1","../images/icons/icon1.jpg","1"]
+                ],[
+                    ["1","../images/icons/icon1.jpg","1"],["2","../images/icons/icon2.jpg","2"]
+                ],[
+                    ["1","../images/icons/icon1.jpg","1"]
+                ],[
+                    ["1","../images/icons/icon1.jpg","1"]
+                ],[
+                    ["1","../images/icons/icon1.jpg","1"],["2","../images/icons/icon2.jpg","2"],["3","../images/icons/icon3.jpg","3"],["4","../images/icons/icon4.jpg","4"]
+                ],[
+                    ["1","../images/icons/icon1.jpg","1"],["2","../images/icons/icon2.jpg","2"]
+                ],[
+                    ["1","../images/icons/icon1.jpg","1"],["2","../images/icons/icon2.jpg","2"]
+                ],[
+                    ["1","../images/icons/icon1.jpg","1"],["2","../images/icons/icon2.jpg","2"]
+                ],[
+                    ["1","../images/icons/icon1.jpg","1"]
+                ],[
+                    ["1","../images/icons/icon1.jpg","1"],["2","../images/icons/icon2.jpg","2"],["3","../images/icons/icon3.jpg","3"],["4","../images/icons/icon4.jpg","4"]
+                ],[
+                    ["1","../images/icons/icon1.jpg","1"],["2","../images/icons/icon2.jpg","2"],["3","../images/icons/icon3.jpg","3"],["4","../images/icons/icon4.jpg","4"]
+                ],[
+                    ["1","../images/icons/icon1.jpg","1"],["2","../images/icons/icon2.jpg","2"],["3","../images/icons/icon3.jpg","3"],["4","../images/icons/icon4.jpg","4"]
+                ],
+            ]};
             data=datas;
             showMap(datas);
             for(i=0;i<data.userMap.length;i++){
-                $("#strategies").append("<div class='province'><span>"+data.userMap[i][2]+"</span><span class='bracket'> (</span><span class='number'> "+data.userMap[i][1]+" </span><span class='bracket'>)</span><span class='iconfont icon-zhankai'></span><span class='iconfont icon-shouqi'></span></div><div class='all' id='all0'></div>");
+                $("#strategies").append("<div class='strategy'><div class='province'><span>"+data.userMap[i][2]+"</span><span class='number'> ( "+data.userMap[i][1]+" ) </span><span class='iconfont icon-zhankai'></span><span class='iconfont icon-shouqi'></span></div><div class='all' id='all"+i+"'></div></div>");
                 for(j=0;j<data.stStrategy[i].length;j++){
-                    $("#all0").append("<div class='each'><img src='"+data.stStrategy[j][0]+"'><div class='title'>"+data.stStrategy[j][1]+"</div>
-                    </div>");
+                    $("#all"+i).append("<div class='each' value='"+data.stStrategy[i][j][0]+"'><img src='"+data.stStrategy[i][j][1]+"'><div class='title'>"+data.stStrategy[i][j][2]+"</div></div>");
                 }
             }
-            $("#strategies .strategy").eq(0).find(".icon-zhankai").hide();
-            $("#strategies .strategy").filter(':not(:eq(0))').find(".icon-shouqi").hide();
+            $("#strategies .strategy").find(".icon-zhankai").hide();
+            //$("#strategies .strategy").filter(':not(:eq(0))').find(".icon-shouqi").hide();
             $("#strategies .strategy .icon-zhankai").off("click").on("click",function(){
                 $(this).hide().siblings().show();
                 $(this).parent().next().show();
@@ -35,20 +71,80 @@ $(function(){
         }
     });
     
+    /**/
     $("#navStrategy").click(function(){
+        $(this).find(".icon").addClass("iconClick");
+        $(this).siblings().find(".icon").removeClass("iconClick");
         $("#strategyContainer").show().siblings().hide();
     });
     
+    /**/
     $("#navCollect").click(function(){
-        $("#collectContainer").show().siblings().hide();
+        $.ajax({
+/*            type:"post",
+            url:"",
+            dataType:"json",*/
+            success:function(data){
+                var data={"collections":[["1","../images/notfound.jpg","title","author","anhui","2018-01-21 12:23:58","1","2"],["1","../images/systemerror.jpg","title1","author1","anhui1","2018-02-21 12:23:58","11","22"]]};
+                $(".collects").empty();
+                if(!data.collections){
+                    $(".collects").append("<span class='nothing'>暂无收藏，去逛逛吧...</span>");
+                    return;
+                }
+                for(i=0;i<data.collections.length;i++){
+                    $(".collects").append("<div class='collect' value='"+data.collections[i][0]+"'><img id='mainImg' src='"+data.collections[i][1]+"'><div class='collectInfo'><span><span class='title'>"+data.collections[i][2]+"</span><span class='author'>"+data.collections[i][3]+"</span><span class='city'>"+data.collections[i][4]+"</span></span><span class='bot'><span class='date'>"+data.collections[i][5]+"</span><span><span class='iconfont icon-zan1'></span><span class='forNum'>"+data.collections[i][6]+"</span><span class='iconfont icon-collection-b'></span><span>"+data.collections[i][7]+"</span></span></span></div></div>");
+                }
+            },
+            error:function(){
+                
+            }
+        });
+        $(this).find(".icon").addClass("iconClick");
+        $(this).siblings().find(".icon").removeClass("iconClick");
+        $("#collectContainer").css("display","flex").siblings().hide();
     });
+    
+    /**/
     $("#navUpload").click(function(){
+        $(this).find(".icon").addClass("iconClick");
+        $(this).siblings().find(".icon").removeClass("iconClick");
         $("#uploadContainer").show().siblings().hide();
     });
+    
+    /**/
     $("#navEdit").click(function(){
-        $("#editContainer").show().siblings().hide();
+        $(this).find(".icon").addClass("iconClick");
+        $(this).siblings().find(".icon").removeClass("iconClick");
+        $("#editContainer").css("display","flex").siblings().hide();
     });
+    $("#editUpdate").click(function(){
+        $(this).addClass("editCilck").siblings().removeClass("editCilck");
+        $(".edit").show().siblings().hide();
+    });
+    $("#editReset").click(function(){
+        $(this).addClass("editCilck").siblings().removeClass("editCilck");
+        $(".reset").show().siblings().hide();
+    });
+    var oldPhone=18366666666;
+    $("#phone").keyup(function(){
+        //todo 3
+        if(oldPhone == $(this).val()){
+            $("#updateBtn").attr("disabled","true");
+        }else{
+            $("#updateBtn").removeAttr("disabled");
+        }
+    });
+    $("#updateBtn").click(function(){
+        $(".codeContainer").show();
+    });
+    $("#resetBtn").click(function(){
+        console.log(123);
+    });
+    
+    /**/
     $("#navMsg").click(function(){
+        $(this).find(".icon").addClass("iconClick");
+        $(this).siblings().find(".icon").removeClass("iconClick");
         $("#msgContainer").show().siblings().hide();
     });
     
@@ -219,4 +315,34 @@ function showMap(datas){
         }]
     };
     myChart.setOption(option);
+}
+
+function getPath(node){
+    var path = "";
+    try{
+        var file = null;
+        if(node.files && node.files[0] ){
+            file = node.files[0];
+        }else if(node.files && node.files.item(0)) {
+            file = node.files.item(0);
+        }
+        //Firefox 因安全性问题已无法直接通过input[file].value 获取完整的文件路径
+        try{
+            //Firefox7.0
+            path =  file.getAsDataURL();
+        }catch(e){
+            //Firefox8.0以上
+            path = window.URL.createObjectURL(file);
+        }
+    }catch(e){
+        //支持html5的浏览器,比如高版本的firefox、chrome、ie10
+        if (node.files && node.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                path = e.target.result;
+            };
+            reader.readAsDataURL(node.files[0]);
+        }
+    }
+    $("#usericon").attr("src",path);
 }
